a1fb1f884c1f935745b8322522c55a8709ef4c10
Mahan Jadiyavar <jdmahan69@gmail.com>
2023-08-04 18:03:55 +0530

    commit

--- js/app.js
+++ js/app.js
@@ -4,15 +4,47 @@ class CalorieTracker{
         this._totalCalories = 0;
         this._meals = [];
         this._workouts = [];
+
+        this._displayCaloriesLimit();
+        this._displayCaloriesTotal();
+        this._displayCaloriesConsumed();
     }
+
+    //  Public methods/API
 
     addMeal(meal) {
         this._meals.push(meal);
         this._totalCalories += meal.calories;
+        this._render();
     }
     addWorkout(workout) {
         this._workouts.push(workout);
         this._totalCalories -= workout.calories;
+        this._render();
+    }
+
+    // Private methods
+
+    _displayCaloriesTotal() {
+        const totalCaloriesEl = document.getElementById('calories-total');
+        totalCaloriesEl.innerHTML = this._totalCalories;
+    }
+
+    _displayCaloriesLimit() {
+        const calorieLimitEl = document.getElementById('calories-limit');
+        calorieLimitEl.innerHTML = this._calorieLimit;
+    }
+
+    _displayCaloriesConsumed() {
+        const caloriesConsymedEl = document.getElementById('calories-consumed');
+
+        const consumed = this._meals.reduce((total, meal) => total + meal.calories, 0);
+        caloriesConsymedEl.innerHTML = consumed;
+    }
+
+    _render() {
+        this._displayCaloriesTotal();
+        this._displayCaloriesConsumed();
     }
 }
 
@@ -37,6 +69,9 @@ const tracker = new CalorieTracker();
 
 const breakfast = new Meal('Breakfast', 400);
 tracker.addMeal(breakfast);
+const lunch = new Meal('Lunch', 399);
+tracker.addMeal(lunch);
+ 
 
 const run = new Workout('Morning Run', 300);
 tracker.addWorkout(run);
